75face761f7193fa3e709d37a52e5d3e090954d5
ushan <ushanchamodbandara@gmail.com>
2022-06-04 15:16:39 +0530

    fix grammer error

--- Connection.py
+++ Connection.py
@@ -1,13 +1,40 @@
 import mysql.connector
+import os
+
+# clear cmd
+clear = lambda: os.system('cls')
 
 class Connection:
-  db_host = 'localhost'
-  db_user = 'root'
-  db_password = ''
-  db_name = 'vehicle_db'
+  try:
+    db_host = 'localhost'
+    db_user = 'root'
+    db_password = ''
+    db_name = 'vehicle_db'
 
-  # open database connection
-  mydb = mysql.connector.connect(host = db_host, user = db_user, password = db_password, database = db_name)
+    # open database connection
+    mydb = mysql.connector.connect(host = db_host, user = db_user, password = db_password, database = db_name)
 
-  # create cursor
-  cursor = mydb.cursor()
+    # create cursor
+    cursor = mydb.cursor()
+  
+  except:
+    clear()
+    print(
+      """
+    + + + + + + + + + + + + + + + + + + +  + +
+    +                                        +
+    +               Error....!               +
+    +                                        +
+    +       DATABASE CONNECTION ERROR        +
+    +                                        +
+    +  01. Check if server is started.       +
+    +                                        +
+    +  02. Check MySQL is working or not.    +
+    +      If not, fix it and try again      +
+    +                                        +
+    + + + + + + + + + + + + + + + + + + +  + + 
+    """
+      )
+
+    exit()
+    
--- Release.py
+++ Release.py
@@ -26,19 +26,19 @@ class Release:
       data = conn.cursor.fetchall()
       conn.mydb.commit()
 
-      print("\nThis is job asign vehicles :-")
+      print("\nThis is job assign vehicles :-")
       print()
 
       for vehicle in data:
         print("  Vehicle Number       : ",vehicle[0])
         print("  Vehicle name         : ",vehicle[1])
-        print("  Number of Passangers : ",vehicle[2])
-        print("  AC Avalibility       : ",vehicle[3])
+        print("  Number of Passengers : ",vehicle[2])
+        print("  AC Availability       : ",vehicle[3])
         print("  Size                 : ",vehicle[4])
         print("------------------------------------")
         print()
       
-      select = input("Which Vehicle You Want Release (Wnter Vehicle ID) : ")
+      select = input("Which Vehicle You Want Release (Enter Vehicle ID) : ")
       try:
         sql = "UPDATE `vehicle` SET `in_job`= 0 WHERE `id` = %s"
         val = (select,)
@@ -59,8 +59,8 @@ class Release:
           pass
 
       except:
-        print("SQL Query Erorr...!")
+        print("SQL Query Error...!")
     except:
       clear()
-      print("Query Erorr..!")
+      print("Query Error..!")
       self.releaseVehical()
